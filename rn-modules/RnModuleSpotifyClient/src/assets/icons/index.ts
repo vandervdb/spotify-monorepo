a5790d910ba95ab2179e2ea4414b551fd66f9d40
Arnaud  Vanderbecq <arnaud.vander@proton.me>
2026-02-16 12:04:08 +0100

    Add `ByVander` SVG component and integrate into `HeaderCard` layout
    - Added `byVander.svg` and included it in `rn-modules/RnModuleSpotifyClient/src/assets/icons/index.ts`.
    - Updated `HeaderCard` component to render the `ByVander` icon with an overlay style.
    - Made adjustments to Gradle properties to disable Android analytics (potential fix for build issues).

--- rn-modules/RnModuleSpotifyClient/src/assets/icons/index.ts
+++ rn-modules/RnModuleSpotifyClient/src/assets/icons/index.ts
@@ -1,1 +1,2 @@
 export {default as SpotifyLogo} from './SpotifyLogo.svg';
+export {default as ByVander} from './byVander.svg';
--- rn-modules/RnModuleSpotifyClient/src/components/HeaderCard.tsx
+++ rn-modules/RnModuleSpotifyClient/src/components/HeaderCard.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Card} from 'react-native-paper';
 import {StyleProps} from 'react-native-reanimated';
 import {SpotifyLogo} from '@spotify/client';
+import {StyleSheet, View} from 'react-native';
+import {ByVander} from '../assets/icons';
 
 export interface HeaderCardProps {
     style?: StyleProps;
@@ -11,9 +13,19 @@ export const HeaderCard = ({style}: HeaderCardProps) => {
         <Card style={[{padding: 0}, style]}>
             <Card.Content>
                 <SpotifyLogo width={240} height={60} />
+                <View style={styles.overlay}>
+                    <ByVander width={240} height={80} />
+                </View>
             </Card.Content>
         </Card>
     );
 };
 
 export default HeaderCard;
+
+const styles = StyleSheet.create({
+    overlay: {
+        ...StyleSheet.absoluteFillObject,
+        pointerEvents: 'none',
+    },
+});
